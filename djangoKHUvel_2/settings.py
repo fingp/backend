f5fc76b39211d73112ec77ddfd2d65b9e08e6d60
Ryulth <adboy94@khu.ac.kr>
2018-09-24 21:22:10 +0900

    mysql setting mysql 설정파일은 슬랙요망

--- djangoKHUvel_2/settings.py
+++ djangoKHUvel_2/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,8 +82,11 @@ WSGI_APPLICATION = 'djangoKHUvel_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': "mysql.cnf",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" # strict mode 설정 추가
+        }
     }
 }
 
