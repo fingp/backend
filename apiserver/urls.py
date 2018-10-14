6f716f5fb27dbc4ad66e36308f7e6b0f06462ab6
Ryulth <adboy94@khu.ac.kr>
2018-10-14 20:05:05 +0900

    delete template

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page),
+    url(r'^$', views.login),
     url('board/', views.board),
     url('board', views.board),
     url('login/', views.login),
